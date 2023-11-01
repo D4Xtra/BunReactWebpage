352738359d9ba3bb1ab476ca930643db6d5d400a
David Kowal <david.kowal@outlook.de>
2023-11-01 16:48:00 +0100

    Started Movie Project, by preparing and cleaning up

--- Front-End/testsite/src/App.js
+++ Front-End/testsite/src/App.js
@@ -1,41 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-const user = {
-  name: 'Hedy Lamarr',
-  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
-  imageSize: 90,
-};
+const App = () => {
+    return (
+        <>
+        <h1>App</h1>
+        <button>test</button>
+        </>
+    );
+}
 
-var isLoggedIn = false;
-
-
-export default function Profile() {
-  let content;
-  if (isLoggedIn) {
-    content = <AdminPanel />;
-  } else {
-    content = <LoginButton />;
-  }
-  return (
-    <>
-      {content}
-    </>
-  );
-};
-
-function AdminPanel() {
-  return (
-    <div>
-      <p>Admin</p>
-    </div>
-  );
-};
-
-function LoginButton() {
-  return (
-    <div>
-      <button>Login</button>
-    </div>
-  );
-};
+export default App;
